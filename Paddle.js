8d70fce41d91662dc59261dc5d5d684d812c44f1
Ant Bal <antbal52@gmail.com>
2024-03-19 18:07:58 -0700

    Update Ball and Paddle

--- Paddle.js
+++ Paddle.js
@@ -1,5 +1,30 @@
+const SPEED = 0.02
+
 export default class Paddle {
-    constructor(PaddleElem) {
-        this.PaddleElem = PaddleElem
-    }
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
+
+  reset() {
+    this.position = 50
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+  }
 }
